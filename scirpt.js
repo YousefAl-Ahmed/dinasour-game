60053e12d2c8edb922c9bb4ad97833ca8db5c188
Bashar-Alhaies <bashar.alhaies@gmail.com>
2021-10-25 02:42:12 +0300

    add block slide

--- scirpt.js
+++ scirpt.js
@@ -1,11 +1,10 @@
 const canvas = document.querySelector('#canvas')
 
 class Block {
- constructor(height, width, x, y) {
-  this.height = height
+ constructor(width, height) {
   this.width = width
-  this.x = x
-  this.y = y
+  this.height = height
+  this.element = document.createElement("div")
  }
 }
 
@@ -18,19 +17,29 @@ class Player {
 }
 
 let player = new Player(20, 20, 40)
+let b1 = new Block(20, 20,)
 let offSetY = 0
 const playerElement = document.createElement("div")
 function renderPlayer() {
  playerElement.style.width = player.width + "px"
  playerElement.style.height = player.height + "px"
- // playerElement.textContent = "HI"
- playerElement.style.display = "block"
  playerElement.style.position = "absolute"
  playerElement.style.bottom = offSetY + "px"
  playerElement.style.left = "2px"
  playerElement.style.backgroundColor = "lightblue"
  console.log(playerElement)
  canvas.appendChild(playerElement)
+}
+
+function renderBlock() {
+ b1.element.style.width = b1.width + "px"
+ b1.element.style.height = b1.height + "px"
+ b1.element.style.position = "absolute"
+ b1.element.style.bottom = 0 + "px"
+ b1.element.style.right = 0 + "px"
+ b1.element.style.backgroundColor = "yellow"
+ canvas.appendChild(b1.element)
+ b1.element.classList.add("slide")
 }
 
 document.addEventListener('keyup', event => {
@@ -50,5 +59,15 @@ playerElement.addEventListener('animationend', () => {
  playerElement.classList.remove('player--jump')
 })
 
-renderPlayer()
 
+function startGame() {
+ renderPlayer()
+ renderBlock()
+}
+
+function endGame() {
+
+}
+
+
+startGame()
